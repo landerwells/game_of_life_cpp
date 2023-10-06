4e8ef52f2b16e835c146952bb93f5a592832262e
landerwells <landerwells@gmail.com>
2023-10-06 17:14:58 -0400

    initial commit

---
+++ Cell.cpp
@@ -0,0 +1,8 @@
+
+class Cell {
+
+}
+
+Cell::Cell() {
+
+}
---
+++ Life.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  const int gameSize = 50;
+
+  int gameBoard[gameSize][gameSize];
+
+  
+  return 0;
+}
+
+
